5767fafa3b057a02620c1112b98881194ee03e71
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2021-01-12 22:22:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
+  console.log("Enter CORS");
   res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, OPTIONS, PUT");
